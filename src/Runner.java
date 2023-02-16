72c260048180335b4b978dd95e671db02b5e8f5e
BT_4N2_33 <llei0111@bths.edu>
2023-02-16 12:33:28 -0500

    added test

--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,7 @@
 public class Runner {
-    //A package cannot be less than a tenth of a pound or smaller than 2 inches on each side
-    Address
+    public static void main(String[] args) {
+        //A package cannot be less than a tenth of a pound or smaller than 2 inches on each side
+        Address yes = new Address("1313 Mockingbird Lane Apt 6A, Springfield, MO 65123");
+        System.out.println(yes);
+    }
 }
